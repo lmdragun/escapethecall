86ea5afe204a251ca361c9c176fae8308d1594fa
Lindsey <lmdragun@gmail.com>
2015-06-04 17:14:42 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ app.use( bodyParser.json() );
 app.set('view engine', 'hbs')
 app.use(express.static( __dirname + '/public'));
 
+
 app.get("/", function( req, res ){
   res.render("index");
 })
 
-server.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("listening on 3000")
 })
-// 
+//
 // app.listen(3000, function(){
 //   console.log("app listening on port 3000")
 // })
